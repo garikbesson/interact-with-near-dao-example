283ea45f81ff221631d435c181d454cb2e4079be
garikbesson <garikbesson@gmail.com>
2023-10-24 12:55:16 +0200

    added minting nft method

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::ext_contract;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, log, near_bindgen, AccountId, Promise, PromiseError};
+use near_sdk::{env, log, near_bindgen, Gas, AccountId, Promise, PromiseError};
 
 // const NFT_CONTRACT: &str = "x.paras.near";
 const NFT_CONTRACT: &str = "paras-token-v2.testnet";
 const YOCTO_NEAR: u128 = 1;
+const TGAS: u64 = 1_000_000_000_000;
 
 // Define the contract structure
 #[near_bindgen]
@@ -28,6 +29,7 @@ impl Default for Contract {
 trait ExternalNftContract {
   fn nft_token(&self, token_id: TokenId) -> Promise;
   fn nft_transfer(&self, receiver_id: AccountId, token_id: TokenId) -> Promise;
+  fn nft_mint(&self, token_series_id: String, receiver_id: AccountId) -> Promise;
 }
 
 // Implement the contract structure
@@ -75,5 +77,32 @@ impl Contract {
     if call_result.is_err() {
       log!("There was an error contacting NFT contract");
     }
+  }
+
+  #[payable]
+  pub fn nft_mint(&mut self, token_series_id: String, receiver_id: AccountId) -> Promise {
+    let promise = ext_nft_contract::ext(self.nft_contract.clone())
+      .with_static_gas(Gas(30*TGAS))
+      .with_attached_deposit(env::attached_deposit())
+      .nft_mint(token_series_id, receiver_id);
+
+    return promise.then( // Create a promise to callback query_greeting_callback
+      Self::ext(env::current_account_id())
+      .with_static_gas(Gas(30*TGAS))
+      .nft_mint_callback()
+    )
+  }
+
+  #[private] // Public - but only callable by env::current_account_id()
+  pub fn nft_mint_callback(&self, #[callback_result] call_result: Result<TokenId, PromiseError>) -> Option<TokenId> {
+    // Check if the promise succeeded
+    if call_result.is_err() {
+      log!("There was an error contacting NFT contract");
+      return None;
+    }
+
+    // Return the token data
+    let token_id: TokenId = call_result.unwrap();
+    return Some(token_id);
   }
 }
