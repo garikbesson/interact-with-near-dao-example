66056d36245564eb5fe873385a2a9dea30c3a5a5
garikbesson <garikbesson@gmail.com>
2023-10-24 05:02:22 +0200

    nft_transfer method

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use near_sdk::{env, log, near_bindgen, AccountId, Promise, PromiseError};
 
 // const NFT_CONTRACT: &str = "x.paras.near";
 const NFT_CONTRACT: &str = "paras-token-v2.testnet";
+const YOCTO_NEAR: u128 = 1;
 
 // Define the contract structure
 #[near_bindgen]
@@ -26,6 +27,7 @@ impl Default for Contract {
 #[ext_contract(ext_nft_contract)]
 trait ExternalNftContract {
   fn nft_token(&self, token_id: TokenId) -> Promise;
+  fn nft_transfer(&self, receiver_id: AccountId, token_id: TokenId) -> Promise;
 }
 
 // Implement the contract structure
@@ -35,7 +37,7 @@ impl Contract {
     let promise = ext_nft_contract::ext(self.nft_contract.clone())
       .nft_token(token_id);
 
-    return promise.then( // Create a promise to callback query_greeting_callback
+    return promise.then( // Create a promise to callback nft_token_callback
       Self::ext(env::current_account_id())
       .nft_token_callback()
     )
@@ -52,5 +54,26 @@ impl Contract {
     // Return the token data
     let token_data: Token = call_result.unwrap();
     return Some(token_data);
+  }
+
+  // Before transfering token in a such way you may need to force user to approve your smart contract on NFT contract.
+  #[payable]
+  pub fn nft_transfer(&mut self, receiver_id: AccountId, token_id: TokenId) -> Promise {
+    let promise = ext_nft_contract::ext(self.nft_contract.clone())
+      .with_attached_deposit(YOCTO_NEAR)
+      .nft_transfer(receiver_id, token_id);
+
+    return promise.then( // Create a promise to callback query_greeting_callback
+      Self::ext(env::current_account_id())
+      .nft_transfer_callback()
+    )
+  }
+
+  #[private] // Public - but only callable by env::current_account_id()
+  pub fn nft_transfer_callback(&self, #[callback_result] call_result: Result<(), PromiseError>) {
+    // Check if the promise succeeded
+    if call_result.is_err() {
+      log!("There was an error contacting NFT contract");
+    }
   }
 }
